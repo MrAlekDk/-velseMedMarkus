07fdcd3ccda6313e4a61a8d465f43bd25c431a65
Alexander Sørensen <alex030t@stud.kea.dk>
2021-02-04 15:46:16 +0100

    Eigth - Proper not done

---
+++ src/Exercise/ProperCase.java
@@ -0,0 +1,32 @@
+package Exercise;
+
+public class ProperCase {
+
+    public ProperCase(){}
+
+
+    public void checkProperCase(String sætning){
+
+
+        String[] ord = sætning.split(" ");
+        String[] fixedSentence = new String[ord.length];
+        for(int i=0;i<ord.length;i++){
+            String wordToCheck = ord[i];
+
+            if(wordToCheck.length()>3){
+                wordToCheck.charAt(0)
+            }
+
+
+            for(int j=0;j<wordToCheck.length();j++){
+                if(wordToCheck.charAt(j)>=101&&wordToCheck.charAt(j)<=132){
+
+                }
+            }
+        }
+
+
+    }
+
+
+}
