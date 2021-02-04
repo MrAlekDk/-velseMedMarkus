8b56dca629dbe34605f2822386977945187a938c
marku <markusharen@gmail.com>
2021-02-04 15:39:48 +0100

    seventh kvadrat.2.420

--- src/Exercise/Kvadrat.java
+++ src/Exercise/Kvadrat.java
@@ -9,15 +9,15 @@ public Kvadrat (){}
 
     public void makeKvadrat() {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Indtast dit tegn: ");
+        String tegn = sc.nextLine();
+        System.out.print("Indtast størelse: ");
         int size = sc.nextInt();
         for (int i = 0; i < size; i++) {
-
-
         for (int j = 0; j < size; j++) {
-            System.out.print(" * ");
-
+            System.out.print(" " + tegn + " ");
         }
-            System.out.println();
+        System.out.println();
     }
     }
 }
