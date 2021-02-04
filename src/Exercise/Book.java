2a2e91935abe538500220cd4cd7046d5d9583a4d
marku <markusharen@gmail.com>
2021-02-04 15:15:48 +0100

    fifth

---
+++ src/Exercise/Book.java
@@ -0,0 +1,47 @@
+package Exercise;
+
+public class Book {
+    private String titel;
+    private int year;
+    private int ISBNNumber;
+
+    public Book(String titel, int year, int ISBNNumber){
+        this.titel = titel;
+        this.year = year;
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    public int getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setISBNNumber(int ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    @Override
+    public String toString() {
+        return "This Book is " +
+                titel +
+                " and was released in " + year +
+                ", ISBNNumber= " + ISBNNumber +
+                '}';
+    }
+}
+}
