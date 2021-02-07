6d9ea5dbaf2fe00b7edcd8f090a13544206d4783
Alexander <alex030t@stud.kea.dk>
2021-02-07 14:14:23 +0100

    Sortering påbegyndt
    Har tilføjet Sortering class og er begyndt på logikken

---
+++ src/Exercise/Sortering.java
@@ -0,0 +1,34 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Sortering implements Comparable{
+    private Scanner input;
+
+    public Sortering(){
+        input = new Scanner(System.in);
+
+    }
+
+    public void sortWords(){
+        String[] words = new String[5];
+
+        for(int i=0;i<5;i++){
+        words[i] = input.nextLine();
+        }
+
+        for(int j=0;j<words.length-1;j++){
+            String word = words[j];
+            if(word.charAt(j)>words[j+1].charAt(0)){
+
+            }
+        }
+
+
+    }
+
+    @Override
+    public int compareTo(Object o) {
+
+    }
+}
